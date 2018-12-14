f76e0fa3f35e0c0e0c895b823505a8911a70d0af
Maxwell Ng-Tiofack <maxwell.ngtiofack@googlemail.com>
2018-12-14 09:05:46 +0100

    Initial commit

---
+++ app/src/test/java/com/example/ng_tiofack/mynews/ConvertDate.java
@@ -0,0 +1,35 @@
+package com.example.ng_tiofack.mynews;
+
+import com.example.ng_tiofack.mynews.utils.Utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ConvertDate {
+
+
+    @Test
+    public void convert_date ()
+    {
+        String date = "2018-10-30T11:30:50-04:00";
+        assertEquals("30/10/2018", Utils.getConvertDate(date));
+
+        date = "2018-10-30";
+        assertEquals("30/10/2018", Utils.getConvertDate(date));
+    }
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+
+
+
+}
